b6b3c0adc36ddbacd6ef69264f006bbae1aeef80
Sushovan <officialkarmakar@gmail.com>
2021-09-26 11:14:58 +0530

    React chart component

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,36 @@
+import "./Chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const Chart = ({ title, data, dataKey, grid }) => {
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <YAxis />
+          <Line
+            type="monotone"
+            dataKey={dataKey}
+            stroke="#5550bd"
+            activeDot={{ r: 8 }}
+          />
+          <Tooltip />
+          <Legend />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../data.js";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        dataKey="Active User"
+        grid={true}
+      />
     </div>
   );
 };
